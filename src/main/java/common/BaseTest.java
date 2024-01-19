6f6110906987071c0e5b2ad3f144ae329ba81121
GraceTran <graceyen261@gmail.com>
2024-01-19 16:08:00 +0700

    second commit

--- src/main/java/common/BaseTest.java
+++ src/main/java/common/BaseTest.java
@@ -3,6 +3,8 @@ package common;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
 
 import java.time.Duration;
 
@@ -39,5 +41,47 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://bl.sotatek.works/");
+    }
+
+    protected boolean verifyTrue(boolean condition) {
+        boolean status = true;
+        try {
+            Assert.assertTrue(condition);
+            System.out.println("---------------------- Passed -----------------------");
+        } catch (Throwable e) {
+            status = false;
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+            System.out.println("---------------------- Failed -----------------------");
+        }
+        return status;
+    }
+
+    protected boolean verifyFalse(boolean condition) {
+        boolean status = true;
+        try {
+            Assert.assertFalse(condition);
+            System.out.println("---------------------- Passed -----------------------");
+        } catch (Throwable e) {
+            status = false;
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+            System.out.println("---------------------- Failed -----------------------");
+        }
+        return status;
+    }
+
+    protected boolean verifyEquals(Object actual, Object expected) {
+        boolean status = true;
+        try {
+            Assert.assertEquals(actual, expected);
+            System.out.println("---------------------- Passed -----------------------");
+        } catch (Throwable e) {
+            status = false;
+            VerificationFailures.getFailures().addFailureForTest(Reporter.getCurrentTestResult(), e);
+            Reporter.getCurrentTestResult().setThrowable(e);
+            System.out.println("---------------------- Failed -----------------------");
+        }
+        return status;
     }
 }
