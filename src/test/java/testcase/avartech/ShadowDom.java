be2adc2fbddabf9fd3228096385c05a1d28183a2
gracetran <graceyen261@gmail.com>
2024-01-23 23:54:44 +0700

    first commit

--- src/test/java/testcase/avartech/ShadowDom.java
+++ src/test/java/testcase/avartech/ShadowDom.java
@@ -1,5 +1,19 @@
 package testcase.avartech;
 
 public class ShadowDom {
+    // Static method
+    static void myStaticMethod() {
+        System.out.println("Static methods can be called without creating objects1");
+    }
 
+    // Public method
+    public void myPublicMethod() {
+        System.out.println("Public methods must be called by creating objects");
+    }
+
+    // Main method
+    public static void main(String[] args) {
+       myStaticMethod(); // Call the static method
+
+    }
 }
